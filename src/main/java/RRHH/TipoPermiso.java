7b3969600b89b40806a3f3b97bdf710189dd79df
Sergi <serchiazu@alu.edu.gva.es>
2025-04-21 20:17:34 +0200

    Subir Enum

---
+++ src/main/java/RRHH/TipoPermiso.java
@@ -0,0 +1,11 @@
+package RRHH;
+
+public enum TipoPermiso {
+    ENFERMEDAD,
+    ENFERMEDAD_FAMILIAR,
+    CUIDADO_HIJO,
+    MATRIMONIO,
+    CONSULTA_MEDICA,
+    VACACIONES
+    // ... otros tipos de permiso
+}
